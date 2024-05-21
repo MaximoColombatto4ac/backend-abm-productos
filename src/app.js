00e99e93d21b6843b4b38a180e93ecbceac49123
MaximoColombatto4ac <maximo.colombatto@gmail.com>
2024-05-21 16:53:23 -0300

    update

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: process.env.ALLOWED_ORIGIN.split(' ') || 'http://localhost:5137',
+    origin: "*",
     credentials: true,
 }));
 app.use(morgan('dev'));
